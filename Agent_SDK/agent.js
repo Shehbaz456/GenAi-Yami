c0982836dc5d6e1c041220394730f7827176ff8d
shehbaz <mdshehbaz666666@gmail.com>
2025-09-02 14:02:33 +0530

    style: format code for consistency in agent.js

--- Agent_SDK/agent.js
+++ Agent_SDK/agent.js
@@ -1,28 +1,27 @@
-import 'dotenv/config';
-import { Agent, run , tool } from '@openai/agents';
-import { z } from 'zod';
+import "dotenv/config";
+import { Agent, run, tool } from "@openai/agents";
 
 // Define a database array to keep track of the conversation
 let database = [];
 
 const customerSupportAgent = new Agent({
-  name: 'Customer Support Agent',
-  model:"gpt-4.1-mini",
+  name: "Customer Support Agent",
+  model: "gpt-4.1-mini",
   instructions: `
     You are a customer support agent who helps users with their queries.
   `,
-  
 });
 
 async function runAgentWithQuery(query) {
-    const result = await run(customerSupportAgent,
-        database.concat({ role: "user", content: query })
-    );
-    database = result.history;
-    // console.log(`Result History`, result.history);
-    console.log(result.finalOutput);
+  const result = await run(
+    customerSupportAgent,
+    database.concat({ role: "user", content: query })
+  );
+  database = result.history;
+  // console.log(`Result History`, result.history);
+  console.log(result.finalOutput);
 }
 
 runAgentWithQuery("Hey there, my name is Shehbaz khan").then(() => {
-    runAgentWithQuery("what is my name?");
+  runAgentWithQuery("what is my name?");
 });
